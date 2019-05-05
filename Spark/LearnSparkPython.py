6af9df84d273abfca758fe4287318e523f6c05e9
Rya Meyvriska <rya.meyvriska@mail.ugm.ac.id>
2019-05-05 10:19:21 +0700

    - delete yang tidak sama dengan di java # ubah nama dataset jadi dataframe soalnya sebenarnya yg di keluarkan adalah dataFrame

--- Spark/LearnSparkPython.py
+++ Spark/LearnSparkPython.py
@@ -9,21 +9,6 @@ sparkConf = SparkConf().setMaster("local").setAppName("Python Spark Playground")
 sparkContext = SparkContext(conf=sparkConf)
 sparkSession = SparkSession(sparkContext)
 
-raw_data = [
-    "rya semangat balajarnya ya",
-    "zetra selalu mendukungmu",
-    "love you",
-    "semangat semangat"
-]
-
-dataset = sparkContext.parallelize(raw_data)
-
-word_counts = dataset.flatMap(lambda sentence: sentence.split(" ")) \
-    .map(lambda word: (word, 1)) \
-    .countByKey()
-
-print(word_counts)
-
 # membuar RDD
 story = "Pada jaman dahulu Zetra belum mandi, sampai sekarang belum mandi. Terus dia merasa gatal, dan akhirnya " \
         "mandi. Selesai. "
@@ -42,10 +27,10 @@ rddFile = sparkContext.textFile("/Users/rya.meyvriska/Downloads/mock_data.csv")
 for v in rddFile.take(3):
     print(v)
 
-# membaca file ke dataset
-datasetFile = sparkSession.read.option("header", "true").csv("/Users/rya.meyvriska/Downloads/mock_data.csv")
-datasetFile.show()
-datasetFile.describe().show()
+# membaca file ke dataframe
+dataFrameFile = sparkSession.read.option("header", "true").csv("/Users/rya.meyvriska/Downloads/mock_data.csv")
+dataFrameFile.show()
+dataFrameFile.describe().show()
 
 
 # menambah column company
@@ -55,7 +40,7 @@ def mapping_function_company(email):
     return sub_by_at[1:sub_by_at.find('.')]
 
 
-datasetFile = datasetFile.withColumn("company", mapping_function_company("email"))
+dataFrameFile = dataFrameFile.withColumn("company", mapping_function_company("email"))
 
 
 # menambah kolom fullname
@@ -64,7 +49,7 @@ def mapping_function_fullname(first_name, last_name):
     return first_name + " " + last_name
 
 
-datasetFile = datasetFile.withColumn("full_name", mapping_function_fullname("first_name", "last_name"))
+dataFrameFile = dataFrameFile.withColumn("full_name", mapping_function_fullname("first_name", "last_name"))
 
 
 # menumerikan gender
@@ -76,8 +61,8 @@ def mapping_function_gender(gender):
         return 1
 
 
-datasetFile = datasetFile.withColumn("gender", mapping_function_gender("gender"))
-datasetFile.show()
+dataFrameFile = dataFrameFile.withColumn("gender", mapping_function_gender("gender"))
+dataFrameFile.show()
 
 # membuat dataframe baru
 schemaGender = StructType([
@@ -88,10 +73,10 @@ dataGender = sparkSession.createDataFrame([(0, "Female"), (1, "Male")], schemaGe
 dataGender.show()
 
 # join dengan table lama
-print(datasetFile.columns)
-datasetFile.join(dataGender, datasetFile.gender == dataGender.id) \
-    .select(datasetFile.id, datasetFile.full_name, dataGender.name.alias("gender")) \
+print(dataFrameFile.columns)
+dataFrameFile.join(dataGender, dataFrameFile.gender == dataGender.id) \
+    .select(dataFrameFile.id, dataFrameFile.full_name, dataGender.name.alias("gender")) \
     .show()
 
 # save csv
-datasetFile.write.option("header", "true").csv("/Users/rya.meyvriska/Downloads/result_pyspark.csv")
+dataFrameFile.write.option("header", "true").csv("/Users/rya.meyvriska/Downloads/result_pyspark.csv")
